7d6f10e757a7c66d3962cdddb6c1e1872d42165b
Ugochukwudouglas <Ugochukwudouglas@ugochukwus-air.hal.can.ibm.com>
2017-03-23 16:00:38 -0300

    Added Classes to Items Package

---
+++ KemiAdesanya/src/KemiAdesanya/Item/HairTies.java
@@ -0,0 +1,15 @@
+/* Copyright (c) Ugochukwu Chukwu
+ /* All Rights Reserved
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package KemiAdesanya.Item;
+
+/**
+ *
+ * @author Ugochukwudouglas
+ */
+public class HairTies extends Product{
+    
+}
